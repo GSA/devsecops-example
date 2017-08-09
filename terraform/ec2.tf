4f71808067320f9983d91a0d283b75fd573c5e62
Aidan Feldman <aidan.feldman@gsa.gov>
2017-08-09 01:34:01 -0400

    add an EC2 instance for Drupal

---
+++ terraform/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_key_pair" "deployer" {
+  key_name_prefix = "deployer-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "drupal" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.drupal_a.id}"
+  key_name = "${aws_key_pair.deployer.key_name}"
+
+  tags {
+    Name = "Drupal"
+  }
+
+  provisioner "local-exec" {
+    command = "echo Successfully connected"
+  }
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {
+  default = "ami-cd0f5cb6"
+  description = "Defaults to Ubuntu Server 16.04 LTS (HVM), SSD Volume Type for us-east-1"
+}
