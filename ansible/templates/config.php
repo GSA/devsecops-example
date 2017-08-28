86eb98ac4c757476c459d6a0c51a5826aad40dbe
Aidan Feldman <aidan.feldman@gsa.gov>
2017-08-28 03:08:49 -0400

    templatize the wordpress config files

--- ansible/templates/config.php
+++ ansible/templates/config.php
@@ -3,5 +3,5 @@ define('DB_NAME', '{{ db_name }}');
 define('DB_USER', '{{ db_user }}');
 define('DB_PASSWORD', '{{ db_pass }}');
 define('DB_HOST', '{{ db_host }}');
-define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+define('WP_CONTENT_DIR', '{{ wp_content_dir }}');
 ?>
