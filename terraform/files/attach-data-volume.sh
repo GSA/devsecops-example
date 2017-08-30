d8203924298783d89d2a517e6af073851bd4543c
Aidan Feldman <aidan.feldman@gsa.gov>
2017-08-30 05:05:18 -0400

    remove need for symlinking volume mounting

--- terraform/files/attach-data-volume.sh
+++ terraform/files/attach-data-volume.sh
@@ -8,9 +8,9 @@ set -x
 
 # note the name doesn't match the device_name in Terraform
 DEVICE=/dev/xvdf
-MOUNT=/usr/share/wordpress/wp-content-mount
 OWNER=www-data
 DEST=/usr/share/wordpress/wp-content
+OLDDEST=$DEST-old
 
 devpath=$(readlink -f $DEVICE)
 
@@ -19,24 +19,22 @@ if [[ $(sudo file -s $devpath) != *ext4* && -b $devpath ]]; then
   sudo mkfs -t ext4 $devpath
 fi
 
-sudo mkdir -p $MOUNT
-sudo chown $OWNER:$OWNER $MOUNT
-sudo chmod 0775 $MOUNT
+sudo mv $DEST $OLDDEST
+sudo mkdir -p $DEST
 
-echo "$devpath $MOUNT ext4 defaults,nofail,noatime,nodiratime,barrier=0,data=writeback 0 2" | sudo tee -a /etc/fstab > /dev/null
-sudo mount $MOUNT
+echo "$devpath $DEST ext4 defaults,nofail,noatime,nodiratime,barrier=0,data=writeback 0 2" | sudo tee -a /etc/fstab > /dev/null
+sudo mount $DEST
+
+sudo chown $OWNER:$OWNER $DEST
+sudo chmod 0775 $DEST
 
 # TODO: /etc/rc3.d/S99local to maintain on reboot
 echo deadline | sudo tee /sys/block/$(basename "$devpath")/queue/scheduler
 echo never | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 
 # if themes directory doesn't exist...
-if [ ! -d "$MOUNT/themes" ]; then
-  # ...volume doesn't contain initial data. Copy existing content in.
+if [ ! -d "$DEST/themes" ]; then
+  # ...volume doesn't contain initial data. Copy initial content in.
   # https://askubuntu.com/a/86891/501568
-  sudo cp -a $DEST/. $MOUNT/
+  sudo cp -a $OLDDEST/. $DEST/
 fi
-
-sudo rm -rf $DEST
-sudo ln -s $MOUNT $DEST
-sudo chown -h $OWNER:$OWNER $DEST
