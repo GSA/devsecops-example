d7a6f1ce51b1aa7d5416495d4f7bda84f225c68a
Aidan Feldman <aidan.feldman@gsa.gov>
2017-08-30 06:09:02 -0400

    move the volume configuration to its own file

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -42,25 +42,3 @@ resource "aws_eip" "public" {
   instance = "${aws_instance.wordpress.id}"
   vpc = true
 }
-
-resource "aws_ebs_volume" "wp_content" {
-  # TODO deletion protection
-  availability_zone = "${data.aws_subnet.public.availability_zone}"
-  size = 10
-}
-
-resource "aws_volume_attachment" "wp_content" {
-  device_name = "/dev/sdf"
-  volume_id = "${aws_ebs_volume.wp_content.id}"
-  instance_id = "${aws_instance.wordpress.id}"
-
-  # https://github.com/hashicorp/terraform/issues/2740#issuecomment-288549352
-  skip_destroy = true
-  provisioner "remote-exec" {
-    script = "files/attach-data-volume.sh"
-    connection {
-      user = "${var.ssh_user}"
-      host = "${aws_eip.public.public_ip}"
-    }
-  }
-}
