f9b8a2ccc7f9a36c82d2e18696c38fbf885a5022
Aidan Feldman <aidan.feldman@gsa.gov>
2017-09-22 22:59:41 -0400

    set required variables
    ...and do other required setup in CircleCI, as well as passing values to
    the networking module. This should fix the build.

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,4 +6,16 @@ module "network" {
 
   mgmt_vpc_name = "devsecops-example"
   private_mgmt_zone_name = "${var.private_zone_name}"
+
+  devsecops_flow_log_policy = "devsecops_example_flow_log"
+  mgmt_private_subnet_cidrs = ["${var.vpc_cidr}"]
+  mgmt_dns_hostnames = "true"
+  mgmt_dns_support = "true"
+  mgmt_database_subnet_cidrs = ["${var.database_subnet_cidrs}"]
+  mgmt_public_subnet_cidrs = ["${var.public_subnet_cidr}"]
+  mgmt_nat_gateway = "true"
+  mgmt_flow_log_group_name = "devsecops_example_flow_log"
+  devsecops_iam_log_role_name = "devsecops_example_flow_log"
+  aws_region = "${data.aws_region.current.name}"
+  mgmt_vpc_cidr = "${var.vpc_cidr}"
 }
