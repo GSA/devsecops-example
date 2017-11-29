68af5df13c6cff5d1ef96de49ee7f71003fcd190
Aidan Feldman <aidan.feldman@gsa.gov>
2017-11-29 12:06:34 -0500

    switch to using IAM role module from cg-provision

--- terraform/mgmt/iam.tf
+++ terraform/mgmt/iam.tf
@@ -1,16 +1,5 @@
-resource "aws_iam_role" "jenkins" {
-  name_prefix = "jenkins_role_"
-  assume_role_policy = "${file("${path.module}/files/assume_role_policy.json")}"
-}
-
-resource "aws_iam_instance_profile" "jenkins" {
-  name_prefix = "jenkins_profile_"
-  role = "${aws_iam_role.jenkins.name}"
-}
-
-// https://adrianhesketh.com/2016/06/27/creating-aws-instance-roles-with-terraform/
-resource "aws_iam_role_policy" "jenkins" {
-  name_prefix = "jenkins_role_policy_"
-  role = "${aws_iam_role.jenkins.id}"
-  policy = "${file("${path.module}/files/role_policy.json")}"
+module "jenkins_master_role" {
+  source = "github.com/18F/cg-provision/terraform/modules/iam_role"
+  iam_policy = "${file("${path.module}/files/role_policy.json")}"
+  role_name = "${var.jenkins_iam_role_name}"
 }
