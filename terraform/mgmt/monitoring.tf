9d40e5dd98ce94c98b25f7833ba5d824f2f75fd7
Aidan Feldman <aidan.feldman@gsa.gov>
2018-01-03 03:18:42 -0500

    remove Jenkins configuration
    Deploying with CircleCI instead, so no longer needed.

--- terraform/mgmt/monitoring.tf
+++ terraform/mgmt/monitoring.tf
@@ -5,14 +5,3 @@ module "alerting" {
   general_availability_protocol = "${var.general_availability_protocol}"
   general_availability_endpoint = "${var.general_availability_endpoint}"
 }
-
-module "os_monitoring" {
-  source = "../modules/os_monitoring"
-
-  instance_id = "${module.jenkins_instances.instance_id}"
-  component = "jenkins"
-  sns_topic_arn = "${module.alerting.sns_topic_arn}"
-  high_cpu_threshold = "${var.high_cpu_jenkins_alarm_threshold}"
-  high_memory_threshold = "${var.high_memory_jenkins_alarm_threshold}"
-  high_disk_util_threshold = "${var.high_disk_util_jenkins_alarm_threshold}"
-}
--- terraform/mgmt/variables.tf
+++ terraform/mgmt/variables.tf
@@ -11,14 +11,6 @@ variable "public_subnet_cidr" {
   default = "10.0.0.0/24"
 }
 
-variable "jenkins_instance_type" {
-  default = "t2.micro"
-}
-
-variable "jenkins_iam_role_prefix" {
-  default = "jenkins_role_"
-}
-
 variable "env_backend_bucket_prefix" {
   default = "devsecops-example-env-"
 }
@@ -35,21 +27,6 @@ variable "general_availability_protocol" {
 variable "general_availability_endpoint" {
   type = "string"
   description = "The destination (such as an email address) to send the monitoring alerts to. More info under https://docs.aws.amazon.com/cli/latest/reference/sns/subscribe.html#options. Note that changing this value will require you to run a 'taint aws_sns_topic.sns_general_availability' before an 'apply'."
-}
-
-variable "high_cpu_jenkins_alarm_threshold" {
-  description = "Percent CPU threshold for alarm on Jenkins instance"
-  default = "80"
-}
-
-variable "high_memory_jenkins_alarm_threshold" {
-  description = "Percent memory threshold for alarm on Jenkins instance"
-  default = "80"
-}
-
-variable "high_disk_util_jenkins_alarm_threshold" {
-  description = "Percent disk utilization threshold for alarm on Wordpress instance"
-  default = "85"
 }
 
 variable "deployer_username" {
