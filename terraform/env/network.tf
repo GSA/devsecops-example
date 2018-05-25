a5a01c962e6fc09c8952a7d79a33587392018d05
Aidan Feldman <aidan.feldman@gsa.gov>
2018-05-25 01:49:47 -0400

    use CIDR function for computing ranges

--- terraform/env/network.tf
+++ terraform/env/network.tf
@@ -1,21 +1,22 @@
 module "network" {
   source = "terraform-aws-modules/vpc/aws"
+
   # Newest v1.11.0 released 12/11/17 fixes additional private subnet issue.
   version = ">= 1.11.0"
 
   azs = [
     "${data.aws_region.current.name}${var.azs[0]}",
-    "${data.aws_region.current.name}${var.azs[1]}"
+    "${data.aws_region.current.name}${var.azs[1]}",
   ]
 
   name = "devsecops-example"
 
   enable_dns_hostnames = true
-  enable_dns_support = true
-  database_subnets = ["${var.database_subnet_cidrs}"]
-  public_subnets = ["${var.public_subnet_cidr}"]
-  enable_nat_gateway = true
-  cidr = "${var.vpc_cidr}"
+  enable_dns_support   = true
+  database_subnets     = ["${cidrsubnet(var.vpc_cidr, 8, 101)}", "${cidrsubnet(var.vpc_cidr, 8, 102)}"]
+  public_subnets       = ["${cidrsubnet(var.vpc_cidr, 8, 0)}"]
+  enable_nat_gateway   = true
+  cidr                 = "${var.vpc_cidr}"
 }
 
 # ensure uniqueness within an account
--- terraform/env/variables.tf
+++ terraform/env/variables.tf
@@ -3,16 +3,8 @@ variable "vpc_cidr" {
 }
 
 variable "azs" {
-  default = ["a", "b"]
+  default     = ["a", "b"]
   description = "Availability zones to use within the specified region - pick two from https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#using-regions-availability-zones-describe"
-}
-
-variable "public_subnet_cidr" {
-  default = "10.0.0.0/24"
-}
-
-variable "database_subnet_cidrs" {
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "ip_whitelist" {
@@ -25,7 +17,7 @@ variable "private_zone_name" {
 
 variable "db_pass" {
   description = "The database password"
-  type = "string"
+  type        = "string"
 }
 
 variable "ssh_user" {
@@ -41,31 +33,31 @@ variable "sns_general_availability_topic_name" {
 }
 
 variable "general_availability_protocol" {
-  default = "email"
+  default     = "email"
   description = "See '--protocol' options under https://docs.aws.amazon.com/cli/latest/reference/sns/subscribe.html#options"
 }
 
 variable "general_availability_endpoint" {
-  type = "string"
+  type        = "string"
   description = "The destination (such as an email address) to send the monitoring alerts to. More info under https://docs.aws.amazon.com/cli/latest/reference/sns/subscribe.html#options. Note that changing this value will require you to run a 'taint aws_sns_topic.sns_general_availability' before an 'apply'."
 }
 
 variable "high_cpu_wordpress_alarm_threshold" {
   description = "Percent CPU threshold for alarm on Wordpress instance"
-  default = "80"
+  default     = "80"
 }
 
 variable "high_memory_wordpress_alarm_threshold" {
   description = "Percent memory threshold for alarm on Wordpress instance"
-  default = "80"
+  default     = "80"
 }
 
 variable "high_disk_util_wordpress_alarm_threshold" {
   description = "Percent disk utilization threshold for alarm on Wordpress instance"
-  default = "85"
+  default     = "85"
 }
 
 variable "deployer_public_key_path" {
-  default = ""
+  default     = ""
   description = "Path to a public key to use for deployment. Defaults to the public key checked into the repository."
 }
